f985ae89e5af01268aac0e7ff8acbbf23def499f
Alec Wilson <alecw1992@gmail.com>
2025-07-19 17:59:00 +0100

    Give administrators group admin access to training account

--- users/infrastructure/data.tf
+++ users/infrastructure/data.tf
@@ -1,3 +1,16 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_ssoadmin_instances" "this" {}
+
+data "aws_organizations_organization" "org" {}
+
+data "aws_organizations_organizational_unit_descendant_accounts" "root" {
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+locals {
+  accounts = {
+    for account in data.aws_organizations_organizational_unit_descendant_accounts.root.accounts :
+    account.name => account
+  }
+}
--- users/infrastructure/permission_sets.tf
+++ users/infrastructure/permission_sets.tf
@@ -12,6 +12,15 @@ resource "aws_ssoadmin_account_assignment" "administrator_access_management" {
   target_type        = "AWS_ACCOUNT"
 }
 
+resource "aws_ssoadmin_account_assignment" "administrator_access_training" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
+  principal_id       = aws_identitystore_group.administrators.group_id
+  principal_type     = "GROUP"
+  target_id          = local.accounts["Training"].id
+  target_type        = "AWS_ACCOUNT"
+}
+
 resource "aws_ssoadmin_managed_policy_attachment" "administrator_access" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
   managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
@@ -19,5 +28,8 @@ resource "aws_ssoadmin_managed_policy_attachment" "administrator_access" {
   # Adding an explicit dependency on the account assignment resource will
   # allow the managed attachment to be safely destroyed prior to the removal
   # of the account assignment.
-  depends_on = [aws_ssoadmin_account_assignment.administrator_access_management]
+  depends_on = [
+    aws_ssoadmin_account_assignment.administrator_access_management,
+    aws_ssoadmin_account_assignment.administrator_access_training
+  ]
 }
