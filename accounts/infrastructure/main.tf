49b563714eda6ba297f6ab59e44de653a591e096
Alec Wilson <alecw1992@gmail.com>
2025-07-19 18:22:08 +0100

    Setup bucket for terraform state in training account

--- accounts/infrastructure/main.tf
+++ accounts/infrastructure/main.tf
@@ -1,6 +1,10 @@
-resource "aws_organizations_account" "account" {
+resource "aws_organizations_account" "training" {
   name              = "Training"
   email             = "alecw1992+awstraining@gmail.com"
   close_on_deletion = true
 }
 
+resource "aws_s3_bucket" "training_terraform_state" {
+  provider      = aws.training
+  bucket_prefix = "terraform-training-bucket-"
+}
--- accounts/infrastructure/providers.tf
+++ accounts/infrastructure/providers.tf
@@ -2,3 +2,10 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+provider "aws" {
+  alias  = "training"
+  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::447839931732:role/OrganizationAccountAccessRole"
+  }
+}
